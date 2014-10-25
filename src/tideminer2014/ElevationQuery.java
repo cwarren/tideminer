3ecd43f513f1c606e4213905627efd02bbbab800
Chris <Chris@MININT-J35RAR8>
2014-10-24 20:25:07 -0400

    initial commit

---
+++ src/tideminer2014/ElevationQuery.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tideminer2014;
+
+/**
+ * This class handles a single elevation about which a flooding query is being made
+ *
+ * @author Chris
+ */
+public class ElevationQuery {
+    private double height;
+    private int floodCount;
+    private double floodDurationSeconds;
+    
+}
---
+++ src/tideminer2014/TideInterval.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tideminer2014;
+
+import java.time.*;
+
+/**
+ *
+ * @author Chris
+ */
+public class TideInterval {
+    private double beginHeight;
+    private double endHeight;
+    private double heightDelta;
+    private double heightDeltaMagnitude;
+    
+    private Instant beginTime;
+    private Instant endTime;
+    private Duration duration;
+}
